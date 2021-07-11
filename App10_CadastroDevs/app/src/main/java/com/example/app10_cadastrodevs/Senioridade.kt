d59883e8b07955253b6c5562777ae3c45559b2a9
victorluadev <streamyodin@gmail.com>
2021-07-10 23:48:46 -0300

    adicionado APP 10

---
+++ App10_CadastroDevs/app/src/main/java/com/example/app10_cadastrodevs/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.example.app10_cadastrodevs
+
+enum class Senioridade(val nome: String) {
+    SENIOR("Senior"),
+    PLENO("Pleno"),
+    JUNIOR("Junior")
+}
---
+++ App10_CadastroDevs/app/src/main/java/com/example/app10_cadastrodevs/Stack.kt
@@ -0,0 +1,7 @@
+package com.example.app10_cadastrodevs
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
